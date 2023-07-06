9736c588299e39155152939a904e988c8598a3c4
digimori <hanaorikuu@gmail.com>
2023-07-06 14:47:42 +0000

    Random colour generator complete - Code comments added to explain each line

--- ColourFlipper/setup/app.js
+++ ColourFlipper/setup/app.js
@@ -1,1 +1,21 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "blue", "pink", "antiquewhite", "aquamarine", "brown"];
+
+//Getting the element with ID of btn
+const btn = document.getElementById('btn');
+
+// Getting the span class with the class of color
+const color = document.querySelector('.color');
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
+
+// Event listener listening for mouse click which then calls an "Anonymous function" and initiates the logic within the code block {}
+btn.addEventListener('click', function() {
+   // Get a ransom number between 0 and 3 as the Array has 4 entries - ie colors[0]
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+})
+
--- ColourFlipper/setup/hex.js
+++ ColourFlipper/setup/hex.js
@@ -1,1 +1,25 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+//Getting the element with ID of btn
+const btn = document.getElementById('btn');
+
+// Getting the span class with the class of color
+const color = document.querySelector('.color');
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
+
+// Event listener listening for mouse click which then calls an "Anonymous function" and initiates the logic within the code block {}
+btn.addEventListener('click', function() {
+    // hex must start with #
+   let hexColour = '#';
+   // For looping through the array upto a value of 6, as there are 6 characters in hex codes
+   for(let i = 0; i < 6; i++) {
+    hexColour += hex[getRandomNumber()] // Appending the values pulled from the for loop
+   }
+   // Changing the body and text content colours on the page by targeting them
+   color.textContent = hexColour;
+   document.body.style.backgroundColor = hexColour;
+})
+
