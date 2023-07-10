6a4c4b1fe347f6cce77a3cfc3af38ca0b895d37c
digimori <hanaorikuu@gmail.com>
2023-07-10 18:33:36 +0000

    Event listener added for all buttons. Saving as a stop point for today.

--- Questions/setup/app.js
+++ Questions/setup/app.js
@@ -1,2 +1,15 @@
+// Using the usual add/remove class list to show things, loop over them for multiples
+// Get the element via queryselector/getElementBy, then add event listeners
+
 //using selectors inside the element
 // traversing the dom
+
+// Select all buttons
+const btns = document.querySelectorAll('.question-btn');
+
+// Loop, function(e) is accessing the event object
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        console.log(e.currentTarget);
+    })
+})
